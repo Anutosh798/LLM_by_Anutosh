b339093a60155eed8f5bf27eb11c24d994f06feb
Anutosh798 <anutoshsatpute@gmail.com>
2026-06-08 00:37:17 +0530

    create layers

--- NN_project.py
+++ NN_project.py
@@ -18,9 +18,55 @@ Y_valid,Y_train=Y_train_full[:5000],Y_train_full[5000:]
 x_test=x_test/255.
 
 # print(len(X_valid))
-plt.imshow(X_train[0],cmap="binary")
-plt.title(Y_train[0])
-plt.xlabel("x axis")
-plt.ylabel("y axis")
-# plt.axis('off')
-plt.show()
+# plt.imshow(X_train[0],cmap="binary")
+# plt.title(Y_train[0])
+# plt.xlabel("x axis")
+# plt.ylabel("y axis")
+# # plt.axis('off')
+# plt.show()
+
+class_names=["T-shirts/top","Trouser","Pullover","Dress","Coat","Sandal","Shirt","Sneaker","Bag","Ankel Boot"]
+# print(class_names[Y_train[0]])
+# print(Y_train[0])# returns 4 means 'coat' in class_names
+
+# print("in x train",x_test.shape)
+# print("in x valid",X_valid.shape)
+
+# n_rows=4
+# n_cols=10
+# plt.figure(figsize=(n_cols*1.2,n_rows*1.2))
+# for row in range(n_rows):
+#     for col in range(n_cols):
+#         index=n_cols*row+col
+#         plt.subplot(n_rows,n_cols,index+1)
+#         plt.imshow(X_train[index],cmap="binary",interpolation="nearest")
+#         plt.axis('off')
+#         plt.title(class_names[Y_train[index]],fontsize=12)
+# plt.subplots_adjust(wspace=0.2,hspace=0.5)
+# plt.show()
+
+Model=keras.models.Sequential()
+Model.add(keras.layers.Flatten(input_shape=[28,28]))
+Model.add(keras.layers.Dense(300,activation='relu'))
+Model.add(keras.layers.Dense(100,activation='relu'))
+Model.add(keras.layers.Dense(10,activation='softmax'))
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# print(Model.layers)
+# print(Model.summary())
+
+# keras.utils.plot_model(Model,"my_fashion_mnist_model.png",show_shapes=True)
+
+hidden1=Model.layers[1]
+print(hidden1.name)
+
+print(Model.get_layer(hidden1.name)  is hidden1)
+
+weights,biases=hidden1.get_weights()
+print(weights.shape)
+# print(weights)
+print(biases)
+print(biases.shape)
